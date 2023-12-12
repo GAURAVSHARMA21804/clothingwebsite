45b777a92b618557934aa56200c23ef758b2883c
GAURAV SHARMA <gauravsharma21804@gmial.com>
2023-12-12 18:52:21 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+                An 
+
+            </p>
+            <p>
+                hi
+            </p>
+        </div>
+
+    </div>
+  )
+}
